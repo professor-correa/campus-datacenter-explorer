439032320a831e1643c0d48cea3fce2e8369e083
Ryan Silva <ryanferreiradasilva2006@gmail.com>
2025-11-28 22:53:37 -0300

    feat: add dropdown service script

--- src/pages/tablet/ryan/tablet.js
+++ src/pages/tablet/ryan/tablet.js
@@ -28,3 +28,22 @@ abrir.addEventListener("click", () => {
     }
 });
 
+// Dropdown da services 
+document.querySelectorAll(".service .dropdown").forEach(dropdown => {
+  dropdown.addEventListener("click", () => {
+    const content = dropdown.nextElementSibling;
+    const arrowDown = dropdown.querySelector("img.block");
+    const arrowUp = dropdown.querySelector("img.none");
+
+    content.classList.toggle("open");
+
+    // alterna ícones
+    if (content.classList.contains("open")) {
+      arrowDown.style.display = "none";
+      arrowUp.style.display = "block";
+    } else {
+      arrowDown.style.display = "block";
+      arrowUp.style.display = "none";
+    }
+  });
+});
