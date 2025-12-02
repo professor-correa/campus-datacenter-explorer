999c85caf29a1977039b2c29516bba354d8d2edb
lyanbrito <lyanbrito613@gmail.com>
2025-12-02 13:38:32 -0300

    feat: created fibra-arm page

--- src/helpers/header.js
+++ src/helpers/header.js
@@ -1,35 +1,9 @@
-class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
-        this.mobileMenu = document.querySelector(mobileMenu);
-        this.navList = document.querySelector(navList);
-        this.navLinks = document.querySelectorAll(navLinks);
-        this.activeClass = "active";
-
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick() {
-        this.navList.classList.toggle(this.activeClass);
-        this.mobileMenu.classList.toggle(this.activeClass);
-    }
-
-    addClickEvent() {
-        this.mobileMenu.addEventListener("click", this.handleClick);
-    }
-
-    init() {
-        if (this.mobileMenu) {
-            this.addClickEvent();
-            console.log("Working")
-        }
-        return this;
-    }
-}
-
-const mobileNavbar = new MobileNavbar(
-    ".mobile-menu-trigger", 
-    ".nav-list",
-    ".nav-list li",
-);
-
-mobileNavbar.init();
+// Header
+// Menu expansível acessível
+const toggle = document.getElementById('menuToggle');
+const header = document.querySelector('.site-header');
+toggle?.addEventListener('click', () => {
+    const expanded = toggle.getAttribute('aria-expanded') === 'true';
+    toggle.setAttribute('aria-expanded', String(!expanded));
+    header.classList.toggle('menu-open');
+});
